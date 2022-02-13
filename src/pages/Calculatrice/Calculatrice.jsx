0535af05a6ae0aff72d7ed7ee67b9c349e7da8c9
LazezBZH <lazez.bzh@caramail.fr>
2022-02-13 18:24:43 +0100

    V3

---
+++ src/pages/Calculatrice/Calculatrice.jsx
@@ -0,0 +1,33 @@
+import "./Calculatrice.css";
+
+export default function Calculatrice() {
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+    return digits;
+  };
+  return (
+    <div className="ma-calculatrice">
+      <div className="calculatrice">
+        <div className="display">
+          <span>(0)</span>0
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>Effacer</button>
+        </div>
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>,</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
